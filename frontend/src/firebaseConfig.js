fc11715a2d1cc7134ffcb52f39ec126e46965434
huzaifaalam1 <huzaifaalam@arizona.edu>
2025-06-30 21:58:07 -0700

    Clean up sensitive files and finalize secure setup

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -3,16 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyD98vwhXfuTEUxRUB4SI8QRLEGIIYGkXz8",
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "uarizona-rewards.firebaseapp.com",
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "uarizona-rewards",
-    storageBucket: "uarizona-rewards.appspot.com",
-    messagingSenderId: "390819291935",
-    appId: "1:390819291935:web:2935f3d39b23df4b9b462a"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { firebaseConfig, auth, db };
+export { firebaseConfig, auth, db };
